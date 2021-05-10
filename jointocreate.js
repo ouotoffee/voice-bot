4980cc674ac479e4cb86638283e23d800b4f8f33
ouotoffee <52788959+ouotoffee@users.noreply.github.com>
2021-05-10 23:22:16 +0800

    Update jointocreate.js
    fix

--- jointocreate.js
+++ jointocreate.js
@@ -113,7 +113,7 @@ module.exports = function (client) {
         await vc.overwritePermissions([
           {
             id: user.id,
-            allow: ['MANAGE_CHANNELS','SPEAK','USE_VAD','CONNECT','VIEW_CHANNEL','MUTE_MEMBERS','DEAFEN_MEMBERS','MOVE_MEMBERS'],
+            allow: ['MANAGE_CHANNELS','SPEAK','USE_VAD','CONNECT','VIEW_CHANNEL','MUTE_MEMBERS','DEAFEN_MEMBERS','MOVE_MEMBERS','STREAM'],
           },
           {
             id: user.guild.id,
